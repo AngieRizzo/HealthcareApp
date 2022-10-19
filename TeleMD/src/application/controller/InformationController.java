4d6f93b260afd679b695d20039cb069024baa7e4
gtcha2 <aron7628@gmail.com>
2022-10-19 00:07:52 -0500

    building blocks
    added ids to first text box included some comments in controller,
    declared model, tested key events. Next steps are creating model,
    creating dictionary for expected infoViews for model, custom object

--- TeleMD/src/application/controller/InformationController.java
+++ TeleMD/src/application/controller/InformationController.java
@@ -1,5 +1,63 @@
 package application.controller;
 
-public class InformationController {
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
 
+//
+
+
+import application.model.Information;
+
+
+
+// this doesn't inherit any classes?
+public class InformationController {
+	
+	//model declaration
+	Information info;
+	
+	
+	@FXML private TextField firstNameTextBox; 
+	// need to add eventListener for screen size changes on the anchor box
+	
+	public void initialize() {
+		//create new model object
+		 info = new Information();
+		
+		
+	}
+	
+	
+	//	attach this to all free texts, 
+	@FXML private void changedText(Event event) {
+//		 insert model call to update property
+		// create model dictionary for inputs, ie name of id
+		// name of id should be == to dictionary input. 
+		// this will allow the model to call functions anonymously depending
+		// on the source of the key event
+		/*
+		 * options in the model should inclued:
+		 * firstNameTextBox
+		 * lastNameTextBox
+		 * street address, model should handle input issues. 
+		 * city, st, zip, --- model should auto update if possible:::
+		 * phone number, could be single entry and model should update the 
+		 * 		insertion of the -
+		 * 
+		 * 
+		 * */
+		
+        System.out.println(((TextInputControl) event.getSource()).getText());
+    }
+	
+//	@FXML private void testAction(Event event) {
+//		 System.out.println("hello");
+//		 
+//       System.out.println(event);
+//   }
+	
+	
+//	@FXML private void 
 }
--- TeleMD/src/application/model/Information.java
+++ TeleMD/src/application/model/Information.java
@@ -1,5 +1,12 @@
 package application.model;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 
 public class Information {
+	
+	
 
 }
